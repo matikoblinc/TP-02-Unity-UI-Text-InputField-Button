9ed2e12860d8577dbbfd31e7f428c102991b5fb5
matikoblinc <78440014+matikoblinc@users.noreply.github.com>
2022-04-19 14:23:33 -0300

    exercise 1 finished

--- Assets/Scripts/Ej01.cs
+++ Assets/Scripts/Ej01.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ej01 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int edadUsuario;
+    public Text miTexto;
+
     void Start()
     {
-        
+        if (edadUsuario >= 18)
+        {
+            miTexto.text = "Es mayor de edad";
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            miTexto.text = "Es menor de edad";
+            Debug.Log("es menor de edad"); 
+        }
     }
 
     // Update is called once per frame
