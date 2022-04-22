10c84e2adb6287980210193fe26922f229a75052
matikoblinc <78440014+matikoblinc@users.noreply.github.com>
2022-04-22 12:19:29 -0300

    fifth video finished

--- Assets/Scripts/Ej02.cs
+++ Assets/Scripts/Ej02.cs
@@ -1,21 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ej02 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    string contraseñaCorrecta;
+    string contraseñaUsuario;
+    public Text ingresoUsuario;
+
     void Start()
     {
-        
+        contraseñaCorrecta = "123456";
     }
 
-    // Update is called once per frame
+ 
     void Update()
     {
     }
         public void validarContrasenia()
         {
+        contraseñaUsuario = ingresoUsuario.text;
+
+        if (contraseñaUsuario == contraseñaCorrecta)
+        {
+            Debug.Log("bienvenido");
+        }
+        else
+        {
+            Debug.Log("contraseña incorrecta");
+        }
 
         }
     
